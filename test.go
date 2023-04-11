2bdf97b885877bb0a53dfddfb52a5f1068f48c04
piscessjh <130340550+piscessjh@users.noreply.github.com>
2023-04-11 17:35:02 +0800

    Update test.go

--- test.go
+++ test.go
@@ -1,13 +1,8 @@
-package main 
+package go 
 import "fmt" 
-func jianfa (n1 int, n2 int) int {
+func Jianfa (n1 int, n2 int) int {
 	var a int = 0
 	a += n1
 	a -= n2
 	return a
-}
-func main() {
-	a := jianfa(-45,31)
-	fmt.Println(a)
-
 }
